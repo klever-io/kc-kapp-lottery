8860fd29a60acdfca582e58823293889a3354f83
emersonfbarros <emersonfb99@gmail.com>
2024-02-14 15:16:46 -0300

    chore: improves code formatting

--- env.ts
+++ env.ts
@@ -1,4 +1,5 @@
-export const SC_ADDRESS = "klv1qqqqqqqqqqqqqpgqatzwz0xx4dujmxwz0qyaknztk4vyrjkwxgds6w630f"
+export const SC_ADDRESS =
+  "klv1qqqqqqqqqqqqqpgqatzwz0xx4dujmxwz0qyaknztk4vyrjkwxgds6w630f";
 export const PROVIDER_URL = ".devnet.klever.finance";
 export const LOTTERY_NAME = "Test1";
 export const LOTTERY_PRICE = 10000;
